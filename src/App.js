01fc33ec97cda097887a9bd09e0f70cbfed5cb23
Yuvisa QP <yuvisa.palomino@gmail.com>
2022-06-11 20:47:31 -0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import { useState } from 'react';
 
-class App extends Component {
-  render() {
+function App() {
+  const [count,setCount]=useState(0);
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{count}</span>
+        <button id="inc" onClick={()=>setCount(count+1)}>Incrementa</button>
       </div>
     );
-  }
 }
 
 export default App;
